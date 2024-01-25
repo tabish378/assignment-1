6c232ac4883211aae5a04a31bbb15e039a69800f
tabish378 <156536322+tabish378@users.noreply.github.com>
2024-01-25 09:53:09 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+// console:
+let isStudent="I'm Intermediate Student by Pre-Engineering";
+let age="18";
+console.log(isStudent);
+console.log(age);
+
+// Alert:
+alert("Hello! Welcome to this web page; I'm syed tabish ali & I'm Also a Developer")
